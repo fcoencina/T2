2e2216f1aeb624a580d3204a63607a15cbf6c02f
Nxisn <106633606+Nxisn1@users.noreply.github.com>
2023-05-14 21:42:12 -0400

    javadoc Door.java

--- Etapa3/Door.java
+++ Etapa3/Door.java
@@ -1,7 +1,15 @@
+/**
+* clase para las puertas
+*/
 public class Door {
     private final DoorView dView;
     private final MagneticSensor magneticSensor;
     private State state;
+    /**
+    * Constructor de la clase puerta, crea puertas
+    * @param sensor Es el sensor magnetico que tiene la puerta
+    * @param view Es la vista gráfica de la puerta
+    */
     public Door(MagneticSensor sensor, DoorView view) {
         magneticSensor = sensor;
         state = State.CLOSE;
@@ -9,6 +17,9 @@ public class Door {
         dView.addMagneticSensorView(magneticSensor.getView());
         dView.setDoorModel(this);
     }
+    /**
+    * Función para cambiar el estado de la puerta, entre abierta y cerrada
+    */
     public void changeState() {
         switch (state) {  // this is the enhanced version of switch statement. It does not require breaks.
             case OPEN, OPENING -> {
@@ -21,6 +32,9 @@ public class Door {
             }
         }
     }
+    /**
+    * Función para obtener el estado de la puerta, entre abierta y cerrada
+    */
     public State getState() {
         return state;
     }
