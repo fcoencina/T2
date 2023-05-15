340e552cc81c1bde2abe65a5a14aecbade9b6080
Nxisn <106633606+Nxisn1@users.noreply.github.com>
2023-05-14 21:52:13 -0400

    Update MagneticSensorView.java

--- Etapa3/MagneticSensorView.java
+++ Etapa3/MagneticSensorView.java
@@ -1,10 +1,15 @@
 import javafx.scene.Group;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
-
+ /**
+    * clase de la vista del sensor magnético
+    */
 public class MagneticSensorView extends Group {
     private final Rectangle switchView;
     private final Rectangle magnetView;
+     /**
+    * constructor del sensor magnético
+    */
     public MagneticSensorView () {
         switchView = new Rectangle(6,6);
         setCloseView();
@@ -12,9 +17,15 @@ public class MagneticSensorView extends Group {
         magnetView.setFill(Color.BLACK);
         getChildren().addAll(magnetView,switchView);
     }
+     /**
+    * Función colocar la vista del sensor en modo cerrado 
+    */
     public void setCloseView(){
         //...
     }
+    /**
+    * Función colocar la vista del sensor en modo abierto 
+    */
     public void setOpenView(){
         //...
     }
