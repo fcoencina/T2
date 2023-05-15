0f438ce3b5606c3ee4c0e026fb614a94e57de805
Nxisn <106633606+Nxisn1@users.noreply.github.com>
2023-05-14 21:50:55 -0400

    Update MagneticSensor.java

--- Etapa3/MagneticSensor.java
+++ Etapa3/MagneticSensor.java
@@ -1,14 +1,30 @@
+/**
+*  Clase Sensor magnético
+*/
 public class MagneticSensor extends Sensor {
     private final MagneticSensorView view;
+    /**
+    * Constructo de la clase Sensor magnético
+    */
     public MagneticSensor(int z){
         super(z);
         view = new MagneticSensorView();
     }
+    /**
+    * Función colocar el sensor en modo abierto 
+    */
     public void setSensorOpen() {
         setClose(false);
     }
+     /**
+    * Función colocar el sensor en modo cerrado 
+    */
     public void setSensorClose() {
         setClose(true);
     }
+    /**
+    * Función obtener la vista del sensor magnético
+    * @return Vista del sensor magnético
+    */
     public MagneticSensorView getView(){ return view;}
 }
