ace9efb6de3c2787497ff781213e65f28592c1c2
Nxisn <106633606+Nxisn1@users.noreply.github.com>
2023-05-14 21:35:39 -0400

    javadoc Central.java

--- Etapa3/Central.java
+++ Etapa3/Central.java
@@ -4,7 +4,9 @@ import javafx.animation.Timeline;
 import javafx.scene.layout.VBox;
 import javafx.util.Duration;
 import java.util.ArrayList;
-
+/*
+* clase Central
+*/
 public class Central {
     private final ArrayList<Sensor> zones;
     private final ArrayList<Person> people;
@@ -12,6 +14,10 @@ public class Central {
     private final Siren siren;
     private final Timeline periodicCheck;
     private final CentralView view;
+    /*
+    * Constructor de la clase Central
+    * @param siren Sirena con la que se creará la central
+    */
     public Central(Siren siren){
         view = new CentralView(this);
         zones = new ArrayList<Sensor>();
@@ -24,9 +30,16 @@ public class Central {
         periodicCheck.setCycleCount(Animation.INDEFINITE);
         periodicCheck.play();
     }
+    /*
+    * Función para obtener la vista
+    * @return La vista de central
+    */
     public VBox getView (){
         return view;
     }
+    /*
+    * Función para verificar las zonas de la central
+    */
     private void checkZones(){
         boolean[] close = checkCloseZones();
         String msg = "Open zone(s): ";
@@ -49,6 +62,10 @@ public class Central {
             }
         }
     }
+    /*
+    * Función que verifica si las zonas están cerradas
+    * @return un boolean que indica si están cerradas
+    */
     private boolean[] checkCloseZones() {
         boolean[] close = {true, true, true};
         for (Sensor sensor : zones) {
@@ -56,6 +73,9 @@ public class Central {
         }
         return close;
     }
+    /*
+    * Función para armar todas las zonas de la central
+    */
     public void armAll() {
         boolean[] close = checkCloseZones();
         String msg = "Open zone(s): ";
@@ -68,16 +88,30 @@ public class Central {
             view.setDisplay(msg);
         }
     }
+    /*
+    * Función para desarmar la alarma
+    */
     public void disarm() {
         state = CentralState.DISARMED;
         view.setDisplay("Alarm was disarmed!");
     }
+    /*
+    * Función para agregar un sensor a la zona
+    * @param s Es el sensor a agregar
+    */
     public void addNewSensor(Sensor s){
         zones.add(s);
     }
+    /*
+    * Función para agregar una persona a la zona
+    * @param p Es la persona a agregar
+    */
     public void addNewPerson(Person p){
         people.add(p);
     }
+    /*
+    * Función para comprobar el estado de la alarma, si está armada o desarmada
+    */
     enum CentralState {
         ALL_ARMED, PERIMETER_ARMED, DISARMED
     }
