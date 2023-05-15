6644081c2b1cb6686614ae4097a3e3a8ac79d599
Nxisn <106633606+Nxisn1@users.noreply.github.com>
2023-05-14 21:54:32 -0400

    Update PIR_Detector.java

--- Etapa3/PIR_Detector.java
+++ Etapa3/PIR_Detector.java
@@ -8,6 +8,15 @@ public class PIR_Detector extends Sensor{
     private int sensing_angle;
     private int sensing_range;
     private int zone;
+    /**
+    * Constructor de detector PIR
+    * @param x osición en el eje x del sensor
+    * @param y Posición en el eje y del sensor
+    * @param direction_angle Dirección del sensor
+    * @param sensing_angle Ángulo de detección del sensor
+    * @param sensing_range Rango de detección del sensor
+    * @param zone Zona en la que esta el sensor 
+    */
     public PIR_Detector(double x, double y, int direction_angle, int sensing_angle, int sensing_range, int zone){
         super(zone);
         this.x = x;
@@ -17,6 +26,9 @@ public class PIR_Detector extends Sensor{
         this.sensing_range = sensing_range;
         pView = new PIR_DetectorView(this.x, this.y, this.direction_angle);
     }
+    /**
+    * Función para verificar zona
+    */
     public void checkZone(){
         System.out.println("All good");
     }
